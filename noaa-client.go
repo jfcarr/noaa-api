a69f1026eac271ca5852b01fe9107bb02eac69de
jfcarr <jfcarr@gmail.com>
2017-07-17 20:39:45 -0400

    Moved service call to its own function.

--- noaa-client.go
+++ noaa-client.go
@@ -12,16 +12,7 @@ type forecastRequest struct {
 	Product, Begin, End, MaxT, MinT string
 }
 
-func main() {
-	fr := forecastRequest{
-		Latitude:  39.44,
-		Longitude: -84.3,
-		Product:   "time-series",
-		Begin:     "2017-07-17T00:00:00",
-		End:       "2017-07-17T20:00:00",
-		MaxT:      "maxt",
-		MinT:      "mint"}
-
+func callService(fr forecastRequest) string {
 	queryString := fmt.Sprintf("lat=%f&lon=%f&product=%s&begin=%s&end=%s&maxt=%s&mint=%s",
 		fr.Latitude, fr.Longitude, fr.Product, fr.Begin, fr.End, fr.MaxT, fr.MinT)
 
@@ -39,6 +30,21 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	return fmt.Sprintf("%s", results)
+}
+
+func main() {
+	fr := forecastRequest{
+		Latitude:  39.44,
+		Longitude: -84.3,
+		Product:   "time-series",
+		Begin:     "2017-07-18T00:00:00",
+		End:       "2017-07-18T20:00:00",
+		MaxT:      "maxt",
+		MinT:      "mint"}
+
+	results := callService(fr)
 
 	fmt.Printf("%s", results)
 }
