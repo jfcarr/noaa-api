e4ed5a20f1068c24b3aa45a31ab6557b250f42f8
jfcarr <jfcarr@gmail.com>
2017-07-28 20:24:57 -0400

    Added command line argument handling.

--- noaa-client.go
+++ noaa-client.go
@@ -1,19 +1,44 @@
 package main
 
+import (
+	"flag"
+)
+
 func main() {
+	var latitudeFlag float64
+	var longitudeFlag float64
+	var startDateTimeFlag string
+	var endDateTimeFlag string
+
+	flag.Float64Var(&latitudeFlag, "latitude", 39.44, "Latitude")
+	flag.Float64Var(&longitudeFlag, "longitude", -84.3, "Longitude")
+	flag.StringVar(
+		&startDateTimeFlag,
+		"startDateTime",
+		"",
+		"Starting date and time of forecast, e.g., 2017-07-29T00:00:00 (default to now)")
+	flag.StringVar(
+		&endDateTimeFlag,
+		"endDateTime",
+		"",
+		"Ending date and time of forecast, e.g., 2017-07-30T00:00:00 (default to max)")
+	flag.Parse()
+
+	// "2017-07-29T00:00:00"
+
 	fr := forecastRequest{
-		Latitude:            39.44,
-		Longitude:           -84.3,
+		Latitude:            float32(latitudeFlag),
+		Longitude:           float32(longitudeFlag),
 		Product:             "time-series",
-		Begin:               "2017-07-26T00:00:00",
-		End:                 "2017-07-26T20:00:00",
+		Begin:               startDateTimeFlag,
+		End:                 endDateTimeFlag,
 		MaxTemperature:      "maxt",
 		MinTemperature:      "mint",
 		ProbabilityOfPrecip: "pop12",
 		SkyCover:            "sky"}
 
 	results := fr.callService()
-	
+
 	formattedResult := parseResults(results)
 
 	// formattedResult.displayResults()
